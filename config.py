1b5e519735dd5e96bec2b42643473cde028591e4
jagruth <jagruthvasa19@gmail.com>
2024-06-29 17:39:14 +0530

    updated config

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+import os
+
 class Config:
-    DB_USERNAME = 'root'
-    DB_PASSWORD = 'password'
-    DB_HOST = 'localhost'
-    DB_PORT = '3306'
-    DB_NAME = 'task_list_finstack'
+    DB_USERNAME = os.getenv('DB_USERNAME', 'root')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_PORT = os.getenv('DB_PORT', '3306')
+    DB_NAME = os.getenv('DB_NAME', 'task_list_finstack')
 
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
